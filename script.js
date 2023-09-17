449ed3fcc671de869db0180452f96b403b0f9117
Vidhya Parmeshwar Kadam <131935689+vidhyakadam@users.noreply.github.com>
2023-09-17 21:11:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 const players = ["John", "Bob", "Alice", "Poppy"];
-
 const person = {
   name: "John Doe",
   age: 80,
 };
 
-// Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
+const team = players; // Reference to the original array players
+const team1 = [...players]; // Copy of the array players
+const cap1 = { ...person }; // Copy of the object person
 
-window.players = players;
-window.person = person;
-window.team = team;
-window.team1 = team1;
-window.cap1 = cap1;
+// Testing modifications
+players.push("Sarah"); // Modifying the original array
+person.name = "Jane"; // Modifying the original object
+
+console.log("Original players array:", players);
+console.log("team array (reference to players):", team);
+console.log("team1 array (copy of players):", team1);
+console.log("Original person object:", person);
+console.log("cap1 object (copy of person):", cap1);
+
